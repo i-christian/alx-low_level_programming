10867529ffa018bc0c3c89b0e7e66af195b3736c
Christian <111076892+i-christian@users.noreply.github.com>
2022-09-08 12:01:50 -0700

    a C program that prints exactly and that piece of art is useful

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<unistd.h>
+#include <stdlib.h>
+/**
+  *main - entry point
+  *Return: Always 1 (success)
+  */
+int main(void)
+{
+	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
