4b4486958f6ad2cba45357ec639fbd438ed885af
Christian <111076892+i-christian@users.noreply.github.com>
2022-09-15 23:48:50 +0200

    Task 5: second review

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
 /**
- * print_most_numbers - read description 
- * Description: a function that prints the numbers, from 0 to 9.
- * Do not print 2 and 4.
- * followed by a new line.
- * Return: 0 (success)
+ * print_most_numbers - a function that prints the numbers, from 0 to 9.
+ * Skips 2 and 4.
+ * 
+ * Return: Always 0
+ *
  */
 void print_most_numbers(void)
 {
-	int num = '0';
+	int i = '0';
 
-	for (; num <= '9'; num++)
+	for (; i <= '9'; i++)
 	{
-		if (num != '2' && num != '4')
-			_putchar(num);
+		if (i != '2' && i != '4')
+			_putchar(i);
 	}
 	_putchar('\n');
 }
