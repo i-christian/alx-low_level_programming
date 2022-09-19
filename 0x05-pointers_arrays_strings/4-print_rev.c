4bdcff3b817cf77446e1a974dfa1f29b9e03c1e2
Christian <111076892+i-christian@users.noreply.github.com>
2022-09-19 14:42:36 +0000

    a function that reverses a string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,12 +11,13 @@
 void print_rev(char *s)
 {
 	int count = 0;
+
 	while (s[count] != '\0')
 	{
 		count++;
 	}
 
-	for (count -=1; count >= 0; count--)
+	for (count -= 1; count >= 0; count--)
 	{
 		_putchar(s[count]);
 	}
